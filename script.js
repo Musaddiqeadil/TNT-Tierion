85322a45db6afe94d1a47be3e1c9f9bfe2903c61
unknown <mouzzamdanishh@gmail.com>
2025-05-10 13:16:10 +0500

    Updated

--- script.js
+++ script.js
@@ -1,38 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuBtn = document.getElementById('menuBtn');
-    const cancelBtn = document.getElementById('cancelBtn');
-    const menuList = document.querySelector('.menu-list');
-    const menuLinks = document.querySelectorAll('.menu-link');
     const navbar = document.querySelector('.Navbar');
 
-    menuBtn.addEventListener('click', () => {
-        menuList.classList.add('show');
-
-        menuBtn.classList.add('hide');
-        menuBtn.classList.remove('show');
-
-        cancelBtn.classList.add('show');
-        cancelBtn.classList.remove('hide');
-    });
-
-    cancelBtn.addEventListener('click', () => {
-        menuList.classList.remove('show');
-        
-        menuBtn.classList.remove('hide');
-        menuBtn.classList.add('show');
-
-
-        cancelBtn.classList.add('hide');
-        cancelBtn.classList.remove('show');
-    });
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            menuList.classList.remove('show');
-            menuBtn.classList.remove('hide');
-        });
-    });
-
+    const toggle = document.querySelector(".toggle");
+    const navRight = document.querySelector(".nav-right");
+    
+    toggle.addEventListener('click', ()=>{
+        toggle.classList.toggle("active")
+        navRight.classList.toggle("active")
+    })
     // Sticky navbar on scroll
     window.addEventListener('scroll', () => {
         if (window.scrollY > 0) {
